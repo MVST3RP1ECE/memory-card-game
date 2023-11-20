1002a9a568573e5c39e6551f685141cc0538ae48
Aleksandr Khudoleev <92752813+MVST3RP1ECE@users.noreply.github.com>
2023-11-20 14:51:10 +0300

    Initial commit
    Create base HTML and CSS. Prepared for JS.

---
+++ MCD/script.js
@@ -0,0 +1,9 @@
+const animationWrapF = document.querySelector(".grid_1")
+
+animationWrapF.addEventListener("click", function(){
+
+animationWrapF.classList.toggle("animation__F");
+console.log(animationWrapF); 
+
+})
+console.log(animationWrapF);
