071aa840be32ded3e35ef91e6831b10d350788fe
Aleksandr Khudoleev <92752813+MVST3RP1ECE@users.noreply.github.com>
2023-11-23 12:56:08 +0300

    Add files via upload

--- MCD/script.js
+++ MCD/script.js
@@ -1,9 +1,12 @@
 const animationWrapF = document.querySelector(".grid_1")
+const grid_2 = document.querySelector(".grid_2");
+const grid__wrapper = document.querySelector(".grid__wrapper");
 
 animationWrapF.addEventListener("click", function(){
+    // grid_2.classList.toggle("wrap_hidden");
+    // grid__wrapper.classList.toggle("modify__grid_F");
 
-animationWrapF.classList.toggle("animation__F");
-console.log(animationWrapF); 
-
+    animationWrapF.classList.toggle("animation__F");
+    console.log(animationWrapF); 
 })
 console.log(animationWrapF);
